3f1706f6ca74b85b6e8891fd2cf78d7e170e910a
faberdev <fabrizio.dev.br@gmail.com>
2024-11-13 18:51:38 -0500

    Add projeto que esqueci de mandar antes XD

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  const currentTime = new Date()
+  const currentHour = currentTime.getHours()
+
+  let greeting = ""
+
+  if (currentHour < 12) {
+    greeting = "Bom dia"
+  } else if (currentHour < 18) {
+    greeting = "Boa tarde"
+  } else {
+    greeting = "Boa noite"
+  }
+</script>
+
+<h1 class="text-3xl font-bold">
+  {greeting}
+</h1>
